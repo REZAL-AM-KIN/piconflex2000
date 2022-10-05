a7fe5039b8b369793da1b18f3b09a48e1df91bad
peufgeul <montoyasamuel2000@gmail.com>
2022-10-05 23:23:49 +0200

    typo

--- PICONFLEX2000-CLIENT/boot.py
+++ PICONFLEX2000-CLIENT/boot.py
@@ -2,14 +2,14 @@ projet_path="/".join(__file__.split("/")[:-2])+"/"
 
 print("Démarrage 'boot.py'")
 exec(open(
-    script_path+'PICONFLEX2000-CLIENT/launch.py').read())  # Permet de se servir directement de toutes les fonctionnalités de la box quand il est lancé (Très utile pour DEV)
+    projet_path+'PICONFLEX2000-CLIENT/launch.py').read())  # Permet de se servir directement de toutes les fonctionnalités de la box quand il est lancé (Très utile pour DEV)
 try:
-    exec(open(script_path+'PICONFLEX2000-CLIENT/init.py').read())  # Effectue les premières communications avec le serveur
+    exec(open(projet_path+'PICONFLEX2000-CLIENT/init.py').read())  # Effectue les premières communications avec le serveur
 except:
-    exec(open(script_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
+    exec(open(projet_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
 while True:  # Boucle infinie du script
     try:
         exec(open(
-            script_path+'PICONFLEX2000-CLIENT/loop.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
+            projet_path+'PICONFLEX2000-CLIENT/loop.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
     except:
-        exec(open(script_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
+        exec(open(projet_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
