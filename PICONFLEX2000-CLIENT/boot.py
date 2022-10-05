ecf5fc843ded4815e8378b08a16cae88d59f7004
peufgeul <montoyasamuel2000@gmail.com>
2022-10-05 23:06:42 +0200

    modification pour path automatique dans boot et launch

--- PICONFLEX2000-CLIENT/boot.py
+++ PICONFLEX2000-CLIENT/boot.py
@@ -1,19 +1,16 @@
-from os import path
-script_dir = path.abspath( path.dirname( __file__ ) )
-print( script_dir )
+script_path="/".join(__file__.split("/")[:-2])+"/"
 
 print("Démarrage 'boot.py'")
 
-
 exec(open(
-    '/home/pi/PICONFLEX2000-CLIENT/launch.py').read())  # Permet de se servir directement de toutes les fonctionnalités de la box quand il est lancé (Très utile pour DEV)
+    script_path+'PICONFLEX2000-CLIENT/launch.py').read())  # Permet de se servir directement de toutes les fonctionnalités de la box quand il est lancé (Très utile pour DEV)
 try:
-    exec(open('/home/pi/PICONFLEX2000-CLIENT/init.py').read())  # Effectue les premières communications avec le serveur
+    exec(open(script_path+'PICONFLEX2000-CLIENT/init.py').read())  # Effectue les premières communications avec le serveur
 except:
-    exec(open('/home/pi/PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
+    exec(open(script_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
 while True:  # Boucle infinie du script
     try:
         exec(open(
-            '/home/pi/PICONFLEX2000-CLIENT/loop.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
+            script_path+'PICONFLEX2000-CLIENT/loop.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
     except:
-        exec(open('/home/pi/PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
+        exec(open(script_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
--- PICONFLEX2000-CLIENT/launch.py
+++ PICONFLEX2000-CLIENT/launch.py
@@ -1,5 +1,6 @@
+script_path="/".join(__file__.split("/")[:-2])+"/"
 print("Demarrage 'launch.py'")
-exec(open('/home/pi/PICONFLEX2000-CLIENT/setting.py').read())
-exec(open('/home/pi/PICONFLEX2000-CLIENT/config.py').read())
-exec(open('/home/pi/PICONFLEX2000-CLIENT/importation.py').read())
-exec(open('/home/pi/PICONFLEX2000-CLIENT/setup.py').read())
+exec(open(script_path+'PICONFLEX2000-CLIENT/setting.py').read())
+exec(open(script_path+'PICONFLEX2000-CLIENT/config.py').read())
+exec(open(script_path+'PICONFLEX2000-CLIENT/importation.py').read())
+exec(open(script_path+'PICONFLEX2000-CLIENT/setup.py').read())
