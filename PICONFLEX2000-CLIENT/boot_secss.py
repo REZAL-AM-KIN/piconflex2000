4a14026f4f2bee21b5fe59752d5ed4942502f16d
peufgeul <montoyasamuel2000@gmail.com>
2022-11-16 15:00:12 +0100

    creation boot_secss

---
+++ PICONFLEX2000-CLIENT/boot_secss.py
@@ -0,0 +1,20 @@
+projet_path="/".join(__file__.split("/")[:-2])+"/"
+
+print("Démarrage 'boot.py'")
+exec(open(
+    projet_path+'PICONFLEX2000-CLIENT/launch.py').read())  # Permet de se servir directement de toutes les fonctionnalités de la box quand il est lancé (Très utile pour DEV)
+
+if config.debugging :
+    print("Attente de 3s")
+    sleep(3)
+
+try:
+    exec(open(projet_path+'PICONFLEX2000-CLIENT/init.py').read())  # Effectue les premières communications avec le serveur
+except:
+    exec(open(projet_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
+while True:  # Boucle infinie du script
+    try:
+        exec(open(
+            projet_path+'PICONFLEX2000-CLIENT/loop_scess.py').read())  # Execution du script se répétant jusqu'à l'arrêt du système
+    except:
+        exec(open(projet_path+'PICONFLEX2000-CLIENT/error.py').read())  # Script de gestion et affichage des erreurs
