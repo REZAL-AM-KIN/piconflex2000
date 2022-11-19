1312d514e004c4a9c098a61f59bae3d03729ef4e
peufgeul <montoyasamuel2000@gmail.com>
2022-11-19 10:38:23 +0100

    changes in text displayed

--- PICONFLEX2000-CLIENT/loop_secss.py
+++ PICONFLEX2000-CLIENT/loop_secss.py
@@ -9,8 +9,12 @@ while True: #Seconde boucle infinie permettant d'utiliser la commande "break" po
 
     if setting.rezalOn:
         if hashCodeType == CRYPT_hashage(config.codeGuinche) or len(SQL_SELECT(QUERRY_getArgent(STRING_uidStrToInt(UID))))>0:
-            hint("CARTE D'ARGENT",4) #Affichage synchronisation
-            sleep(2)
+            hint("CARTE D'ARGENT",4)
+            sleep(0.5)
+            hint("",4)
+            sleep(0.5)
+            hint("CARTE D'ARGENT",4)
+            sleep(1)
             break
 
         # syncronisation des données sur la carte
@@ -24,14 +28,13 @@ while True: #Seconde boucle infinie permettant d'utiliser la commande "break" po
         # ecriture de l'UID dans la BDD
         SQL_EXECUTE(QUERRY_updateUIDcarteCommande(STRING_uidStrToInt(UID)))
         hint("BDD SYNC",3) #Affichage synchronisation
-        hint("ATTENTE COMMANDES",4) #Affichage synchronisation
 
         #attente du retrait de la carte
         while RFID_carteCheck():
+            hint("ATTENTE COMMANDES",4)
             sleep(0.5)
-            hint("",4) #Affichage synchronisation
+            hint("",4)
             sleep(0.5)
-            hint("ATTENTE COMMANDES",4) #Affichage synchronisation
 
         SQL_EXECUTE(QUERRY_clearUIDcarteCommande())
         hint("BDD CLEAN",3) #Affichage synchronisation
@@ -41,4 +44,4 @@ while True: #Seconde boucle infinie permettant d'utiliser la commande "break" po
         hint("PERTE DU REZAL", 3)  # Affichage du problème
         hint("REBOOT DANS 3s", 4)
         sleep(3)
-        REZAL_restart()  # Redémarrage du système
+        REZAL_restart()  # Redémarrage du système (on est des gros bourin ici)
